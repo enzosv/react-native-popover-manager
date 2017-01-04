70f09c47388b3219b9a4a77946656f2cfe5fea1e
Enzo Vergara <vergara.enzo@gmail.com>
2017-01-04 16:33:59 +0800

    Initial Commit

---
+++ PopoverManager/PopoverManager.h
@@ -0,0 +1,13 @@
+//
+//  PopoverManager.h
+//  PopoverManager
+//
+//  Created by Lorenzo Rey Vergara on Jan 4, 2017.
+//  Copyright © 2017 By Implication, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PopoverManager : NSObject
+
+@end
---
+++ PopoverManager/PopoverManager.m
@@ -0,0 +1,13 @@
+//
+//  PopoverManager.m
+//  PopoverManager
+//
+//  Created by Lorenzo Rey Vergara on Jan 4, 2017.
+//  Copyright © 2017 By Implication, Inc. All rights reserved.
+//
+
+#import "PopoverManager.h"
+
+@implementation PopoverManager
+
+@end
